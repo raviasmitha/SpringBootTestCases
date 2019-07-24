4133f623b32ad810e1717f2ed0c2330c13b827c5
raviasmitha <asmithachowdary1511@gmail.com>
2019-07-24 18:33:14 +0530

    lombok commit

--- src/main/java/com/stackroute/SpringBootTask/domain/Muzix.java
+++ src/main/java/com/stackroute/SpringBootTask/domain/Muzix.java
@@ -1,11 +1,18 @@
 package com.stackroute.SpringBootTask.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Muzix {
 
     @Id
@@ -15,55 +22,4 @@ public class Muzix {
     private int ratings;
     private String comment;
 
-    public Muzix() {
-    }
-
-    public Muzix(int id, String name, int ratings, String comment) {
-        this.id = id;
-        this.name = name;
-        this.ratings = ratings;
-        this.comment = comment;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getRatings() {
-        return ratings;
-    }
-
-    public void setRatings(int ratings) {
-        this.ratings = ratings;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Muzix{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", ratings=" + ratings +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
 }
